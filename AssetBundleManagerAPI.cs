2b1a76524e69ee27d4d1d2d2388e85823206b935
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-10 17:15:49 -0800

    ..

--- AssetBundleManagerAPI.cs
+++ AssetBundleManagerAPI.cs
@@ -1,9 +1,16 @@
 ﻿using System;
-using PartyGame.Core.Scripts;
 
 namespace AssetBundles
 {
   public class AssetBundleManagerAPI : IAssetBundleManagerAPI
   {
+    void IAssetBundleManagerAPI.LoadAssetAsync<TAsset>(
+      string assetBundleName, 
+      string assetName, 
+      Action<TAsset> onLoad)
+    {
+      AssetBundleManager.instance.LoadAssetAsync<TAsset>(
+        assetBundleName, assetName, onLoad);
+    }
   }
 }
